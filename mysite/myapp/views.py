979a127228cd33a9be16cd832cd406b9c5ba0865
Mbelenga <mbelengaterence@gmail.com>
2024-11-03 23:36:03 +0300

    updated

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,5 +1,28 @@
 from django.shortcuts import render
 
-# Create your views here.
+import requests
+from django.shortcuts import render
+
+def index(request):
+    weather_data = {}
+    
+    if 'city' in request.GET:
+        city = request.GET['city']
+        api_key =
+        url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
+        
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            weather_data = {
+                'city': data['name'],
+                'temperature': data['main']['temp'],
+                'description': data['weather'][0]['description'],
+                'icon': data['weather'][0]['icon'],
+            }
+        else:
+            weather_data = {'error': 'City not found'}
+
+    return render(request, 'weather_app/index.html', {'weather_data': weather_data})
 def home_page(request):
     return render(request, 'base.html')

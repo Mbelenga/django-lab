1b25c96435626f58dfacc90df68c672506f40034
Mbelenga <mbelengaterence@gmail.com>
2025-06-16 01:40:57 +0300

    Http Response

--- myprojetc/myapp/views.py
+++ myprojetc/myapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    pass
+    return HttpResponse('<h1> Hi, Welcome To The World Of Django </h1>')

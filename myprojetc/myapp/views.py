6c69405c67981f348179b06fc305786028ffca2c
Mbelenga <mbelengaterence@gmail.com>
2025-06-16 19:27:08 +0300

    made the settings file see the templates folder

--- myprojetc/myapp/views.py
+++ myprojetc/myapp/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('<h1> Hi, Welcome To The World Of Django </h1>')
+    return render(request, 'index.html')

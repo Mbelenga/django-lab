d903320532df77da21e33e598ad00ede1eef466a
Mbelenga <mbelengaterence@gmail.com>
2025-06-18 00:11:47 +0300

    seeing how the backend of my Django app works

--- myprojetc/myapp/views.py
+++ myprojetc/myapp/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     name = 'MALI'
-    return render(request, 'index.html' {'name': name})
+    return render(request, 'index.html', {'name': name})

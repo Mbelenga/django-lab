86783d7aea1358c2043debe2a2bd942256f30803
Mbelenga <mbelengaterence@gmail.com>
2025-06-18 00:14:31 +0300

    updated

--- myprojetc/myapp/views.py
+++ myprojetc/myapp/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    name = 'MALI'
+    name = 'Terrence'
     return render(request, 'index.html', {'name': name})

358b5fee82f10f12837f72ce36dc214b7f7a9eb2
Mbelenga <mbelengaterence@gmail.com>
2025-06-18 23:44:08 +0300

    changing something

--- myprojetc/myapp/views.py
+++ myprojetc/myapp/views.py
@@ -7,5 +7,5 @@ def index(request):
 
 def counter(request):
     text = request.GET['text']
-    amount_of_words = len(text.split())
-    return render(request, 'counter.html', {'amount': amount_of_words})
+    number_of_words = len(text.split())
+    return render(request, 'counter.html', {'number': number_of_words})

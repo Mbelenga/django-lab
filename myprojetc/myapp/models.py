f8e824004c259c28be5d8ef6bba570b49d493833
Mbelenga <mbelengaterence@gmail.com>
2025-07-18 02:12:11 +0300

    Models

--- myprojetc/myapp/models.py
+++ myprojetc/myapp/models.py
@@ -3,3 +3,7 @@ from django.db import models
 # Create your models here.
 # Here we also create our databases
 # Models are also used in configuring our databases
+class Feature:
+    id: int
+    name: str
+    details: str

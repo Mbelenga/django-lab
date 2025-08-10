8200cd5dd81de3def9d4a354f8f15531d203899b
Mbelenga <mbelengaterence@gmail.com>
2025-08-11 01:51:39 +0300

    Good

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utilis import timezone
 
+
 class Post(models.Model):
     class Status(models.TextChoices):
         DRAFT = 'DF', 'Draft'
@@ -12,14 +13,16 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
+
 class Meta:
     ordering = ['-publish']
     """
     We indicate descending order by using a hyphen before the field name
-    We use the ordering attribute to tell Django that it should sort 
+    We use the ordering attribute to tell Django that it should sort
     results by the publish field.
     """
     indexes = [models.Index(fields=['-publish']),]
-    
+
+
 def __str__(self):
     return self.title

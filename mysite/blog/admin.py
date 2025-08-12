32dbdf165a9def4c793a74273e04fe4cd86aa990
Mbelenga <mbelengaterence@gmail.com>
2025-08-12 23:35:00 +0300

    updated

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'author', 'publish', 'status']
 
-# Register your models here.
+admin.site.register(Post)

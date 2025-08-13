a5d784bc90d284f7b2f3cc7845d78ce6ce9be4d0
Mbelenga <mbelengaterence@gmail.com>
2025-08-13 23:52:47 +0300

    updated

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -4,8 +4,8 @@ from django.http import Http404
 
 
 def post_list(request):
-    posts = Post.published.all() return render(
-        request, 'blog/post/list.html', {'posts': posts})
+    posts = Post.published.all()
+    return render(request, 'blog/post/list.html', {'posts': posts})
 
 
 def post_detail(request, id):
